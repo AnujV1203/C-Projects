0b78f7bc96458d3949196581f0d2f593332e2d53
Anuj Vastani <130305405+AnujV1203@users.noreply.github.com>
2023-07-26 20:52:14 +0530

    Addition of two numbers
    We have taken two inputs from user and print the addition of two numbers.

---
+++ Addition.cpp
@@ -0,0 +1,12 @@
+//Addition of two numbers
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a,b,c;
+	cout<<"Enter the two numbers";
+	cin>>a>>b;
+	c=a+b;
+	cout<<"Addition of two numbers is :"<<c;
+	return 0;
+}
