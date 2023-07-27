335e58dadca37e674573cf89818b17aaae44c705
Anuj Vastani <130305405+AnujV1203@users.noreply.github.com>
2023-07-27 15:29:20 +0530

    Evaluation of even or odd number
    We are using if else to check whether is even or odd by dividing a number by 2.If remainder is 0 then it is even number or else odd number.

---
+++ Evenodd.cpp
@@ -0,0 +1,18 @@
+//Evaluation of  Number
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a;
+    cout<<"Enter a number";
+    cin>>a;
+	if (a%2==0)
+	{
+		cout<<"Even Number";
+	}
+    else
+    {
+        cout<<"Odd number";
+    }
+	return 0;
+}
