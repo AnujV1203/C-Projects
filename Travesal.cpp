13430dda666cd0bd0dff6b3f3d5593cd83f25aaa
Anuj Vastani <130305405+AnujV1203@users.noreply.github.com>
2023-07-28 15:28:34 +0530

    Travel of n number
    We are traversing numbers from 1 to n by using for loop and by "endl" we are making all numbers in vertical manner.

---
+++ Travesal.cpp
@@ -0,0 +1,16 @@
+//Travesing of number from 1 to n in vertical manner
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+
+    cout<<"Enter a number";
+    cin>>n;
+
+    for(int i=1;i<n;i++)
+    {
+        cout<<i<<endl;
+    }
+    return 0;
+}
