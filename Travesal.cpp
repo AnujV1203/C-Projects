ac45bc233673a5dcbeec87d1f16840b5d83bc9f4
Anuj Vastani <130305405+AnujV1203@users.noreply.github.com>
2023-07-28 06:15:02 +0530

    Traversal of n numbers
    We are traversing numbers from 1 to n in vertical manner using for loop and using endl keyword such that each new number is on new line.

---
+++ Travesal.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+//Travesing of number from ! to n in vertical manner
+using namespace std;
+int main()
+{
+    int n;
+
+    cout<<"Enter a number";
+    cin>>n;
+
+    for(int i=1;i<n;i++)
+    {
+        cout<<i<<endl;
+    }
+    return 0;
+}
