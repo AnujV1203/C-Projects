6ac40215e68f61ff90e8e9404c960766f684b4c3
Anuj Vastani <130305405+AnujV1203@users.noreply.github.com>
2023-08-03 12:12:18 +0530

    Nested Loop
    In this code we are using for loop i.e. Nested loop to print the specific pattern of numbers.

---
+++ Nestedloop5.cpp
@@ -0,0 +1,16 @@
+
+//Nested loop
+#include<iostream>
+using namespace std;
+int main()
+{
+	for(int i=1;i<=4;i++)
+	{
+		for(int j=1;j<=i;j++)
+		{
+			cout<<i;
+		}
+		cout<<endl;
+	}
+	return 0;
+}
