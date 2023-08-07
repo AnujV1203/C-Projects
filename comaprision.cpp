66620a2286a857225bc4787670789dbc999c02d0
Anuj Vastani <130305405+AnujV1203@users.noreply.github.com>
2023-08-07 09:15:37 +0530

    Program to read two numbers and print the greatest number.
    In this code we are taking two input from user and by using if_else loop to print the greater number among two number.

---
+++ comaprision.cpp
@@ -0,0 +1,16 @@
+//Program to read two integers and print the greater
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a,b;
+	cout<<"Enter two numbers";
+	cin>>a>>b;
+	if(a>b)
+	{
+		cout<<a;
+	}
+	else{
+		cout<<b;
+	}
+}
