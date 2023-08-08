999bc23daec16595b507a37f835d56e81fbaec3c
Anuj Vastani <130305405+AnujV1203@users.noreply.github.com>
2023-08-08 09:22:06 +0530

    Program to read three numbers and print the greatest number among them.
    In this code we are taking three inputs from users and applying if, else_if, else loop to check three numbers and print the largest number.

---
+++ comparision1.cpp
@@ -0,0 +1,28 @@
+//Program to read three numbers and print the largest number
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int a,b,c;
+
+	cout<<"Enter three numbers:";
+	cin>>a>>b>>c;
+
+	if(a>b && a>c)
+	{
+		cout<<a;
+	}
+
+	else if(b>a && b>c)
+	{
+		cout<<b;
+	}
+
+	else
+	{
+		cout<<c;
+	}
+
+	return 0;
+}
