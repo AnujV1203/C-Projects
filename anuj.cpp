12f507fd243ce88962566148afa6bd7cc14c71f5
Anuj Vastani <130305405+AnujVastani@users.noreply.github.com>
2023-08-10 05:11:25 +0530

    Program to print 'Anuj-The Lion' five times using while loop.
    In this program we are using while loop to print my name 5 times.

---
+++ anuj.cpp
@@ -0,0 +1,14 @@
+//Program to print 'Anuj-The Lion'five times using while loop
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a;
+	a=1;
+	while(a<6)
+	{
+		cout<<"Anuj-The Lion"<<endl;
+		a++;
+	}
+	return 0;
+}
