75ef6ad7db4cb9a2030498277e34b95b2089ca95
Anuj Vastani <130305405+AnujV1203@users.noreply.github.com>
2023-08-09 12:22:58 +0530

    Program to check whether entered leap year or not
    In this program we are if_else loop to check the year. If the entered year by user is divisible by 4 then it is leap year otherwise not.

---
+++ leapyear.cpp
@@ -0,0 +1,17 @@
+//Program to chech for a leap year
+#include<iostream>
+using namespace std;
+int main()
+{
+	int year;
+	cout<<"Enter a year:";
+	cin>>year;
+
+	if(year%4==0)
+	{
+		cout<<"Leap Year";
+	}
+	else{
+		cout<<"Not a leap year";
+	}
+}
