125d0ad240f2f278608fb6ede1c3192b61ff678a
Anuj Vastani <130305405+AnujVastani@users.noreply.github.com>
2023-08-11 15:24:03 +0530

    Program to generate all integers from 1 to 10 using while loop.
    In this code we are using while loop to  print numbers from 1 to10 .

---
+++ whileloop.cpp
@@ -0,0 +1,14 @@
+//Program to generate all interegers from 1 to 10 using while loop
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a;
+	a=1;
+	while(a<=10)
+	{
+		cout<<a<<endl;
+	     a++;
+	}
+	return 0;
+}
