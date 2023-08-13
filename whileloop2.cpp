cff408c7900e9808c9e95a01602d43e8333e95f4
Anuj Vastani <130305405+AnujVastani@users.noreply.github.com>
2023-08-13 09:13:42 +0530

    Program to print pattern using while loop.
    In this code we are using while loop to print a pattern
    ****
    ****
    ****

---
+++ whileloop2.cpp
@@ -0,0 +1,16 @@
+//Program to print he given below pattern-
+// ****
+// ****
+// ****
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a=1;
+	while(a<=3)
+	{
+		cout<<"****"<<endl;
+		a++;
+	}
+	return 0;
+}
