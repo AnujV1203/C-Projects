114d5775f9dc4c1bb0f265dec0c0a9eb07156f95
Anuj Vastani <130305405+AnujVastani@users.noreply.github.com>
2023-09-02 23:05:01 +0530

    //Programme that read a value and return square.
    In this code we are taking input from user and multiplying twice the number to give output as a square.

---
+++ square.cpp
@@ -0,0 +1,14 @@
+//Programme that read a value and return square.
+#include<iostream>
+using namespace std;
+int main()
+{
+	int n,s;
+	cout<<"Emter a number";
+	cin>>n;
+
+	s=n*n;
+	cout<<"Square of a number is-"<<s;
+
+	return 0;
+}
