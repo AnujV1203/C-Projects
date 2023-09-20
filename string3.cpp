a48513891a701578a9290393a32593280185df2f
Anuj Vastani <130305405+AnujVastani@users.noreply.github.com>
2023-09-20 09:48:20 +0530

    strcmp()
    In this code we are using string function which is strcmp() which helps to compare two strings.

---
+++ string3.cpp
@@ -0,0 +1,16 @@
+//strcmp
+#include <cstring>
+#include <iostream>
+using namespace std;
+
+int main() {
+  char str1[] = "Megadeth";
+  char str2[] = "Metallica";
+
+  // compare str1 and str2 lexicographically
+  int result = strcmp(str1, str2);
+
+  cout << result;
+
+  return 0;
+}
